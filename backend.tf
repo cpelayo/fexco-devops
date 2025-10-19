cbacb9094841a22e0b82764f00c56f08e873816a
cpelayo <cpr_cesar@hotmail.com>
2025-10-19 16:16:10 +0100

    backend-typo

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstatecpelayostorage" 
-    container_name        = "tfstate"
-    key                   = "fexco-devops.tfstate"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatecpelayostorage"
+    container_name       = "tfstate"
+    key                  = "fexco-devops.tfstate"
   }
 }
