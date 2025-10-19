66dcede7dce85bdca0ea302b3835cac024599e4e
cpelayo <cpr_cesar@hotmail.com>
2025-10-19 12:23:20 +0100

    providers

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
 provider "azurerm" {
   features {}
+  subscription_id = "0d0d36f2-1f7f-49e5-9188-1e3cf650ebd1"
 }
