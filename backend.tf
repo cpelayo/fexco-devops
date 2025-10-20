7d5803d528508f1d992fb75a75a2f29b026fd737
cpelayo <cpr_cesar@hotmail.com>
2025-10-20 13:29:52 +0100

    tcloud-backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,18 @@
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "tfstate-rg"
+#    storage_account_name = "tfstatecpelayostorage"
+#    container_name       = "tfstate"
+#    key                  = "fexco-devops.tfstate"
+#  }
+#}
+
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatecpelayostorage"
-    container_name       = "tfstate"
-    key                  = "fexco-devops.tfstate"
+  backend "remote" {
+    organization = "cpelayo-devops"
+
+    workspaces {
+      name = "fexco-devops"
+    }
   }
 }
