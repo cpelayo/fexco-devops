8d81b97f95106d5c068bd00d3d046e22b1233b44
cpelayo <cpr_cesar@hotmail.com>
2025-10-22 08:19:44 +0100

    code-cleanup

--- providers.tf
+++ providers.tf
@@ -17,52 +17,6 @@ provider "azurerm" {
   subscription_id = "0d0d36f2-1f7f-49e5-9188-1e3cf650ebd1"
 }
 
-/*
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-demo-cluster" # your actual cluster name
-  resource_group_name = "aks-demo-rg"      # your actual resource group
-}
-
-provider "kubernetes" {
-  alias = "aks"
-  host  = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  alias = "aks"
-  kubernetes = {
-    host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-  }
-}
-
-
-provider "kubernetes" {
-  alias                  = "aks"
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  alias = "aks"
-  kubernetes = {
-    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-  }
-}
-
-*/
-
 provider "kubernetes" {
   alias = "aks"
 
